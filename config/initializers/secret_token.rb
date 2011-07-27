51e2dd9a825ca3494ff69553a6f817e445d8f48a
Sergei O. Udalov <sergei.udalov@gmail.com>
2011-07-27 19:31:37 +0400

    rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SketchyCalendarService::Application.config.secret_token = '7c21d037791c4dbe06dc8db381fe73ce7b70c3d83559d3f4a3bc5d35d5dc6f30c13e671ac3b7f1c849c8d8e96bf8e680fd05485592515a41c7c7a458454a4022'
